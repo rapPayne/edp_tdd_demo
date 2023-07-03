7e58c68705def818495d462f5166e111c6a73a60
Jordan <jpessman22@gmail.com>
2023-07-03 14:50:44 -0400

    added tests

--- roman-numeral-convertor/src/romanNumeralConvter.test.js
+++ roman-numeral-convertor/src/romanNumeralConvter.test.js
@@ -1,11 +1,20 @@
+import toRoman from "./romanNumeralConverter";
 
 test('can run a smoke test', () => {
   expect(1).toBe(1)
 })
+
 
 test('given 1 returns "i"', () => {
   const expected = "i";
   const inputArabic = 1;
   const actual = toRoman(inputArabic);
   expect(actual).toBe(expected);
+})
+
+test('converting numeral 5 to string capital V', () => {
+  const expected = "V";
+  const inputArabic = 5;
+  const actual = toRoman(inputArabic);
+  expect(actual).toBe(expected)
 })
