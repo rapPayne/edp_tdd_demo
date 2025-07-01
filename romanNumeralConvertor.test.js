0d9ea797da95f82254a3c0a93f8b4c4dec24d34b
rapPayne <bob@creator.net>
2025-07-01 13:26:47 -0500

    minor change

--- romanNumeralConvertor.test.js
+++ romanNumeralConvertor.test.js
@@ -5,12 +5,12 @@ describe('Roman numberal convertor', () => {
     expect(romanToArabic).toBeTruthy()
   })
 
-  test("returns 16 given XVI", () => {
+  it("returns 16 given XVI", () => {
     const arabic = romanToArabic("XVI");
     expect(arabic).toBe(16);
   })
 
-  test("returns 5 given V", () => {
+  it("returns 5 given V", () => {
     // ARRANGE
     const expected = 5
     // ACT
