324e133c8781592bbf71b02f6a744bdeaa77e6ef
Sam Morgan <smm2311@columbia.edu>
2025-07-01 13:42:54 -0400

    Added failing test.

--- romanNumeralConvertor.test.js
+++ romanNumeralConvertor.test.js
@@ -3,3 +3,8 @@ import { romanToArabic } from "./romanNumeralConvertor";
 test("runs", () => {
   expect(romanToArabic).toBeTruthy()
 })
+
+test("takes roman and returns arabic", () => {
+  const arabic = romanToArabic("XVI");
+  expect(arabic).toBe(16);
+})
